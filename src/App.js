439852400da9063dfde224c1c676b21ad7345c8a
CJ Lee <ldslcj@gmail.com>
2021-05-25 15:54:31 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import BookForm from './BookAdd';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [books, setBooks] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [showForm, setShowForm] = useState(false)
+
+    useEffect(() => {
+    getBookList()
+    }, [])
+
+
+    const getBookList = async () => {
+    console.log('booklist is loading... 2 second delay')
+    let response = await axios.get('https://fakerapi.it/api/v1/books?_quantity=5?delay=2')
+    console.log('after axios call')
+    console.log(response.data)
+    console.log(response.data.data)
+    setBooks(response.data.data)
+    setLoading(false)
+    }
+
+    const deleteBook = (title) => {
+      console.log('deleteBook clicked title:', title)
+      let newBooks = books.filter( book => {
+        return book.title !== title
+      })
+
+      setBooks(newBooks)
+
+    }
+
+    const renderBookList = () => {
+      if(loading){
+        return <p>Loading Book List.... Please wait...</p>
+      }
+      return books.map(book => {
+        return (
+          <div>
+            
+            <p>
+              <img src={book.image}></img><br></br>
+              <b>Book Title:</b> {book.title}<br></br>
+              <b>Author:</b> {book.author}<br></br>
+              <b>Genre:</b> {book.genre}<br></br>
+              <b>ISBN:</b> {book.isbn}<br></br>
+              <button onClick={() => {deleteBook(book.title)}}>Delete</button>
+            
+            </p>
+            <p></p>
+            <p></p>
+          </div>
+        )
+      })
+    }
+
+    const addBook = (book) => {
+      console.log(book)
+      setBooks([book, ...books])
+    }
+
+
+    console.log('about to render to DOM')
+    return (
+      <div className="App">
+        <h1>Books</h1>
+        <button onClick={() => setShowForm(!showForm)}>
+          {showForm ? "Hide Form" : "Add New Book"}</button>
+
+        {showForm && <BookForm addBook = {addBook} />}
+        {renderBookList()}
+      </div>
+    )
 }
 
 export default App;
---
+++ src/BookAdd.js
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+
+const BookForm = (props) => {
+    const [book_title, set_book_title] = useState('')
+    const [book_author, set_book_author] = useState('')
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('submit')
+        console.log(book_title)
+        console.log(book_author)
+        let newBook = {title:book_title, author:book_author, isbn: Math.random()}
+        props.addBook(newBook)
+    }
+    return(
+        <div>
+            <h1>Add New Book</h1>
+            <form onSubmit = {handleSubmit}>
+                <p>Book Title</p>
+                <input
+                    value = {book_title}
+                    onChange = {(e) => set_book_title(e.target.value)} />
+                <p>Author</p>
+                <input
+                    value = {book_author}
+                    onChange = {(e) => set_book_author(e.target.value)} />
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default BookForm
